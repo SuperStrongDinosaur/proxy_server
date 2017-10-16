3bebc230545ebc62d705479a7e96d13cba96bd2c
SuperStrongDinosaur <alex_shirv@mail.ru>
2017-10-16 21:37:16 +0300

    add some lost files

--- source/proxy.cpp
+++ source/proxy.cpp
@@ -18,7 +18,11 @@
 
 #include "proxy.h"
 #include "exceptions.h"
-#include "http_utils.h"
+
+const std::string BAD_REQUEST = "HTTP/1.1 400 Bad Request\r\nServer: proxy\r\nContent-Type: text/html; charset=utf-8\r\nContent-Length: 164\r\nConnection: close\r\n\r\n<html>\r\n<head><title>400 Bad Request</title></head>\r\n<body bgcolor=\"white\">\r\n<center><h1>400 Bad Request</h1></center>\r\n<hr><center>proxy</center>\r\n</body>\r\n</html>";
+
+static const time_t TIMEOUT = 10000;
+
 
 proxy_server::proxy_server(int port) : works(true), want_to_stop(false), queue(), resolver(), proxy_fd(create_listening_socket(port)) {
     int fds[2];
